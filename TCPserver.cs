670dafbf0c679e304e4c689062f41296480a4886
EsraBoztoprak <202306372@post.au.dk>
2024-11-25 16:57:06 +0100

    Add files via upload
    koden

---
+++ TCPserver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            Console.WriteLine("Server started...");
+
+            while (true)
+            {
+                using (TcpClient client = server.AcceptTcpClient())
+                {
+                    NetworkStream stream = client.GetStream();
+                    // Simuler sensor data
+                    string sensorData = "Sensor data: " + DateTime.Now;
+                    byte[] data = Encoding.UTF8.GetBytes(sensorData);
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine($"Sent: {sensorData}");
+                }
+            }
+        }
+
+    }
+}
+
+
